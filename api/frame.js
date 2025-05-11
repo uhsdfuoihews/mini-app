bde16f1a8e4a4135a4354d42952be8ab9bd79bb6
uhsdfuoihews <kevsnedeker@gmail.com>
2025-05-10 21:29:44 -0600

    Update frame.js

--- api/frame.js
+++ api/frame.js
@@ -6,6 +6,7 @@ export default async function handler(req, res) {
       <head>
         <title>My Farcaster Mini App</title>
         <meta property="fc:frame" content="vNext" />
+        <meta property="og:title" content="My First Farcaster Frame" />
         <meta property="fc:frame:image" content="https://res.cloudinary.com/dsjgj0vma/image/upload/v1740849866/good_morning_rjbsdo.png" />
         <meta property="fc:frame:button:1" content="Click Me!" />
         <meta property="fc:frame:post_url" content="https://your-vercel-url.vercel.app/api/action" />
