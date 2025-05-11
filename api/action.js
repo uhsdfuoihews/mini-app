651bb6f4365073226eff5e44754763e06d45919e
uhsdfuoihews <kevsnedeker@gmail.com>
2025-05-10 21:31:36 -0600

    Update action.js

--- api/action.js
+++ api/action.js
@@ -4,10 +4,11 @@ export default async function handler(req, res) {
     <!DOCTYPE html>
     <html>
       <head>
+        <meta property="og:title" content="You Clicked the Button!" />
         <meta property="fc:frame" content="vNext" />
-        <meta property="fc:frame:image" content="https://your-vercel-url.vercel.app/thankyou.png" />
+        <meta property="fc:frame:image" content="https://res.cloudinary.com/dsjgj0vma/image/upload/v1740849866/good_morning_rjbsdo.png" />
         <meta property="fc:frame:button:1" content="Go Back" />
-        <meta property="fc:frame:post_url" content="https://your-vercel-url.vercel.app/api/frame" />
+        <meta property="fc:frame:post_url" content="https://mini-app-mauve-iota.vercel.app/api/frame" />
       </head>
       <body></body>
     </html>
