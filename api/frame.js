7c27fcf9e26594d5db9cf82f28c27914d1bfac2c
uhsdfuoihews <kevsnedeker@gmail.com>
2025-05-10 21:44:12 -0600

    Update frame.js

--- api/frame.js
+++ api/frame.js
@@ -5,13 +5,18 @@ export default async function handler(req, res) {
     <html>
       <head>
         <title>My Farcaster Mini App</title>
-        <meta property="fc:frame" content="vNext" />
-        <meta property="og:title" content="My First Farcaster Frame" />
-        <meta property="fc:frame:image" content="https://res.cloudinary.com/dsjgj0vma/image/upload/v1740849866/good_morning_rjbsdo.png" />
-        <meta property="fc:frame:button:1" content="Click Me!" />
-        <meta property="fc:frame:post_url" content="https://mini-app-mauve-iota.vercel.app/api/action" />
+        <meta name="fc:frame" content="vNext" />
+        <meta name="og:title" content="My First Farcaster Frame" />
+        <meta name="fc:frame:image" content="https://res.cloudinary.com/dsjgj0vma/image/upload/v1740849866/good_morning_rjbsdo.png" />
+        
+        <meta name="fc:frame:button:1" content="Click Me!" />
+        <meta name="fc:frame:button:1:action" content="post" />
+
+        <meta name="fc:frame:post_url" content="https://mini-app-mauve-iota.vercel.app/api/action" />
       </head>
-      <body></body>
+      <body>
+        <p>Frame loaded.</p>
+      </body>
     </html>
   `);
 }
